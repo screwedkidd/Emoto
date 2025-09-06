e0445dc23649420de6db4203258f438b979692e3
screwedkidd <ivanpalichev@gmail.com>
2025-09-06 18:39:03 +0500

    Added primary colors to Color.kt

--- app/src/main/java/com/example/emoto/ui/theme/Color.kt
+++ app/src/main/java/com/example/emoto/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.emoto.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Black = Color(0xFF232323)
+val White = Color(0xFFFFFFFF)
+val PastelPink = Color(0xFFFFC5D4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val MilkyMatcha = Color(0xFFDBE38D)
+val TeaGreen = Color(0xFF698041)
